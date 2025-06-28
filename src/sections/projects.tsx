05a789bed7eff918f43a78cabddb9c9a97db9ea4
DionWsdm <dionwisdom29@gmail.com>
2025-06-28 22:40:33 +0700

    feat: projects section

---
+++ src/sections/projects.tsx
@@ -0,0 +1,11 @@
+import { StarsBg } from "../components/stars"
+
+export function Projects() {
+    
+    return (
+        <div className="flex flex-col w-7xl mx-auto">
+            <StarsBg />
+            <p className="text-5xl font-bold">My Projects</p>
+        </div>
+    )
+}
