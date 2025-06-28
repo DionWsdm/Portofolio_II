97510ca4ab4c8e7eeb9f21adc250b656fd2e72f4
DionWsdm <dionwisdom29@gmail.com>
2025-06-28 22:38:18 +0700

    fix: reversed bug in mobile view

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,11 +1,12 @@
-export function Navigation() {
-    
+export function Navigation({isFlipped, setIsFlipped} : {isFlipped: boolean, setIsFlipped: React.Dispatch<React.SetStateAction<boolean>>}) {
     return (
-        <div className="w-full flex flex-row gap-5 lg:flex-row-reverse lg:gap-15">
-            <div><a href="#project">Project</a></div>
-            <div><a href="#experiences">Experiences</a></div>
-            <div><a href="#about-me">About Me</a></div>
-            <div><a href="#profile">Profile</a></div>
+        <div className="relative w-full flex lg:flex-row-reverse">
+            <div className="absolute z-10 w-full flex justify-between flex-row-reverse lg:w-110 text-[14px] lg:text-[16px]">
+                <button >Project</button>
+                <a href="#experiences">Experiences</a>
+                <button className="hover:cursor-pointer" onClick={() => setIsFlipped(true)}>About Me</button>
+                <button className="hover:cursor-pointer" onClick={() => setIsFlipped(false)}>Profile</button>
+            </div>
         </div>
     )
 }
