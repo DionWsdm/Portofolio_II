ef0f9639ec92ba49245369aa26e8154abbdf6f23
DionWsdm <dionwisdom29@gmail.com>
2025-06-28 22:37:51 +0700

    add needed css style and change About section to Projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { Background } from './sections/background'
 import { Profile } from './sections/profile'
+import { About } from './sections/projects'
 import './App.css'
 
 function App() {
+
+
   return (
-    <div className='flex flex-col w-90 lg:w-7xl'>
-      <Background />
+    <div className='flex flex-col gap-20 w-full mx-auto font-roboto overflow-hidden'>
       <Profile />
+      <About />
     </div>
-    
-    
   )
 }
 
