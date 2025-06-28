f82e52e5bc9523dba4b1225f04a35343b29f2f73
DionWsdm <dionwisdom29@gmail.com>
2025-06-28 22:42:17 +0700

    Change About section to Projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import { Profile } from './sections/profile'
-import { About } from './sections/projects'
+import { Projects } from './sections/projects'
 import './App.css'
 
 function App() {
-
-
   return (
     <div className='flex flex-col gap-20 w-full mx-auto font-roboto overflow-hidden'>
       <Profile />
-      <About />
+      <Projects />
     </div>
   )
 }
