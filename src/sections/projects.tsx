2fbce220d8ed10b83a5f5c114814f4a06aa77761
DionWsdm <dionwisdom29@gmail.com>
2025-07-01 03:49:19 +0700

    added a project

--- src/sections/projects.tsx
+++ src/sections/projects.tsx
@@ -1,25 +1,29 @@
 import { StarsBg } from "../components/stars";
-import Porto from "../assets/porto-ii.png";
+import porto from "../assets/porto-ii.png";
+import compmed from "../assets/compmed.png";
 import Github from "../assets/github.png";
 
 interface project {
     image: string,
     title: string,
     description: string,
+    href: string,
 }
 
 export function Projects() {
     const projects: project[] = [
         {
-            image: Porto,
+            image: porto,
             title: "Portofolio Website",
             description: "A simple website where I share my journey, work, and interests in the field of software engineering.",
+            href: "https://github.com/DionWsdm/Portofolio_II",
         },
-        // {
-        //     image: "",
-        //     title: "",
-        //     description: "",
-        // },
+        {
+            image: compmed,
+            title: "CompMed",
+            description: "This project is a recruitment assignment from compfest 17. A simple social media website where I chose X as a reference.",
+            href: "https://github.com/DionWsdm/CompMed-FE"
+        },
         // {
         //     image: "",
         //     title: "",
@@ -39,7 +43,7 @@ export function Projects() {
                             <p className="text-[18px] lg:text-2xl font-bold">{project.title}</p>
                             <p className="text-[14px] lg:text-[16px]">{project.description}</p>
                             <div className="flex w-full flex-row-reverse">
-                                <a href="https://github.com/DionWsdm/Portofolio_II" className="flex flex-row bg-blue-500/50 w-25 h-9 rounded-[5px] items-center justify-center gap-2">
+                                <a href={project.href} className="flex flex-row bg-blue-500/50 w-25 h-9 rounded-[5px] items-center justify-center gap-2">
                                     <img src={Github} alt="github" className="size-5"/>
                                     <p>GitHub</p>
                                 </a>
